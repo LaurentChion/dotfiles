1fefca53e13fe149c0804b23b7ccde5f673965c9
Laurent <laurent.chion@gmail.com>
2019-12-11 22:45:05 +0100

    font installation

---
+++ 2_font.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+
+# Download font and store it
+mkdir ~/.fonts
+curl -L -o ~/.fonts/Delugia.Nerd.Font.Complete.ttf https://github.com/adam7/delugia-code/releases/download/v1911.21/Delugia.Nerd.Font.Complete.ttf
+
+echo "Font installed, remember to setup inside your CODE editor"
+echo "Font name: 'Delugia Nerd Font'" 
